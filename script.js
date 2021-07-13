687ebc7a642bcc9dd13784349d4930c7e3ef39c4
Tierra <50035689+RuckerTierra@users.noreply.github.com>
2021-07-12 22:22:26 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+// alert("page connected");
+
+
+// console.log();
+
+function printName(first, last){
+  console.log(`My first name is ${first} and my last name is ${last}`);
+}
+
+printName("Tierra", "Rucker");
